4a89f01c29776bd616e7725944282d7593be82f7
Chilja Speransky <magiclanzelot@gmx.net>
2018-09-07 09:45:13 +0200

    MVC structure added

---
+++ js/app.js
@@ -0,0 +1,21 @@
+$(function(){
+
+  var model = {
+
+  };
+
+  var controller = {
+    init: function() {
+      view.init();
+    },
+  };
+
+  var view = {
+    init: function() {
+
+    },
+
+   };
+
+  controller.init();
+});
